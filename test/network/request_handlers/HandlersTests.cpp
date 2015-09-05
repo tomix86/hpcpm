bbcc5c47272bd94c16c4225cdffcc179ba56b68a
Tomasz Gajger <gajger.tomasz@gmail.com>
2015-09-05 22:36:51 +0200

    Fix for two previously DISABLED tests and major changes in CMakeLists
    Change-Id: I6bd5aa5343a8ed1a96b26d166da8d44f029f4ece

--- test/network/request_handlers/HandlersTests.cpp
+++ test/network/request_handlers/HandlersTests.cpp
@@ -3,17 +3,29 @@
 std::streambuf* RequestHandlersTestSuite::oldCoutBuf;
 std::ostringstream RequestHandlersTestSuite::sink;
 
-//TODO: re-enable disabled tests, for now they crash probably due to gmock and gtest incompatibility with
-//our code, possible solution is to recompile gtest and gmock using compilation flags from this project
-//https://stackoverflow.com/questions/31643068/gmock-segmentationfault-on-expect-call
-TEST_F( RequestHandlersTestSuite, DISABLED_ResponseTest ) {
+//This class is needed for ResponseTest and MultipleQueries test
+//those tests had been crashing due to an issue with gmock (EXPECT_CALL), which I was unable to track down
+class MockQueryExecutor2 : public core::QueryExecutor {
+public:
+	MockQueryExecutor2( std::shared_ptr<MockQueryResult> result ) :
+	core::QueryExecutor{ std::make_shared<MockDevicesManager>() },
+	executionResult{ result } {
+	}
+
+	core::QueryHandler::Result::Ptr execute( Query query ) {
+		(void)query;
+		return executionResult;
+	}
+
+	std::shared_ptr<MockQueryResult> executionResult;
+};
+
+TEST_F( RequestHandlersTestSuite, ResponseTest ) {
+	auto queryResult = std::make_shared<MockQueryResult>();
+	MockHandler handler( std::make_shared<MockQueryExecutor2>( queryResult ) );
+
 	EXPECT_CALL( handler, splitIntoQueries( testing::_ ) )
 		.WillOnce( testing::Return( std::vector<Query>{ Query{ Query::Type::GetNodeInformation } } ) );
-
-	auto queryResult = std::make_shared<MockQueryResult>();
-
-	EXPECT_CALL( *queryExecutor.get(), execute( testing::_ ) )
-		.WillOnce( testing::Return( queryResult ) );
 
 	http_response serializationResult( status_codes::OK );
 	serializationResult.set_body( U( "Misza" ) );
@@ -27,19 +39,14 @@ TEST_F( RequestHandlersTestSuite, DISABLED_ResponseTest ) {
 	ASSERT_STREQ( "Misza", result.extract_string().get().c_str() );
 }
 
-TEST_F( RequestHandlersTestSuite, DISABLED_MultipleQueriesTest ) {
+TEST_F( RequestHandlersTestSuite, MultipleQueriesTest ) {
+	auto queryResult = std::make_shared<MockQueryResult>();
+	MockHandler handler( std::make_shared<MockQueryExecutor2>( queryResult ) );
+
 	EXPECT_CALL( handler, splitIntoQueries( testing::_ ) )
 		.WillOnce( testing::Return( std::vector<Query>{ 5, Query{ Query::Type::GetNodeInformation } } ) );
 
-	auto queryResult = std::make_shared<MockQueryResult>();
-	EXPECT_CALL( *queryExecutor, execute( testing::_ ) )
-		.Times( 5 )
-		.WillRepeatedly( testing::Return( queryResult ) );
-
 	EXPECT_CALL( handler, serializeQueriesResults( std::vector<core::QueryHandler::Result::Ptr>{ 5, queryResult } ) );
-
-	EXPECT_CALL( *queryResult, serialize() )
-		.WillOnce( testing::Return( "test" ) );
 
 	ASSERT_NO_THROW( handler.handle( http_request{} ) );
 }
