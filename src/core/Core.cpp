270d18e8975c3b90e9e7284099ae9b815ea2ddae
Tomasz Gajger <gajger.tomasz@gmail.com>
2015-09-08 19:34:14 +0200

    Added SIGINT interception and handling
    Change-Id: I5a02cc3c06b6bbb64f807e01c45728f28341055f

--- src/core/Core.cpp
+++ src/core/Core.cpp
@@ -1,11 +1,17 @@
+#include <atomic>
+#include <csignal>
 #include "Core.hpp"
 #include "utility/Logging.hpp"
 #include "utility/make_unique.hpp"
 
 namespace core {
+std::atomic<bool> running{ true };
+
 core::Core::Core() :
 devicesManager { std::make_shared<devices::DevicesManager>() },
 requestListener{ std::make_shared<QueryExecutor>( devicesManager ) } {
+	signal( SIGINT, []( int ){ running = false; } );
+
 	devicesManager->init();
 	devicesManager->updateDevicesList();
 }
@@ -14,7 +20,12 @@ utility::ApplicationExitCode Core::run( void ) {
 	LOG ( INFO ) << "Core.run()";
 
 	requestListener.start();
-	while ( true );
+	while ( true ) {
+		if ( !running ) {
+			LOG ( INFO ) << "SIGINT caught, terminating.";
+			break;
+		}
+	}
 	requestListener.stop();
 
 	return utility::EXIT_SUCCESSFUL;
