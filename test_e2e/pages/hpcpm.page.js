8165ac669b10c15eb912eda900452767d1ac39c5
Andrzej Podgorski <andrzej.podgorski@hotmail.com>
2015-12-03 16:47:49 +0100

    more e2e
    Change-Id: Ie8e74d04c524335e84a8d68a1c7fb4b306198fe5

--- test_e2e/pages/hpcpm.page.js
+++ test_e2e/pages/hpcpm.page.js
@@ -13,14 +13,37 @@ var HPCPMPage = function () {
         get: function () {
             return element(by.css('[ng-controller="NodesController"]')).element(by.css('[ng-table="tableParams"]')).all(by.repeater('row in $data'))
         },
-        getNodeRow: function (nodeName) {
-            return thisPage.nodesTable.get().filter(function (row) {
-                return row.all(by.tagName('td')).get(0).getText().then(function (txt) {
-                    return txt === nodeName;
-                });
-            }).then(function (elem) {
-                return elem[0];
-            });
+        nodeRow: function (nodeName) {
+            return {
+                get: function () { //unfortunately returns a promise
+                    return thisPage.nodesTable.get().filter(function (row) {
+                        return row.all(by.tagName('td')).get(0).getText().then(function (txt) {
+                            return txt === nodeName;
+                        });
+                    }).then(function (elem) {
+                        return elem[0];
+                    });
+                },
+                nameField: function () {
+                    return thisPage.nodesTable.nodeRow(nodeName).field(0)
+                },
+                field: function (number) {
+                    return {
+                        get: function () {
+                            return thisPage.nodesTable.nodeRow(nodeName).get().then(function (row) {
+                                return row.all(by.tagName('td')).get(number)
+                            })
+                        },
+                        link: {
+                            click: function () {
+                                thisPage.nodesTable.nodeRow(nodeName).field(number).get().then(function (field) {
+                                    field.element(by.tagName('a')).click()
+                                });
+                            }
+                        }
+                    }
+                }
+            }
         }
     };
 
@@ -41,22 +64,64 @@ var HPCPMPage = function () {
         },
         nodeName: {
             set: function (name) {
-                 thisPage.modal.get().element(by.model('node.name')).sendKeys(name)
+                thisPage.modal.get().element(by.model('node.name')).sendKeys(name)
             }
         },
         nodeAddress: {
             set: function (address) {
-                 thisPage.modal.get().element(by.model('node.address')).sendKeys(address)
+                thisPage.modal.get().element(by.model('node.address')).sendKeys(address)
             }
         },
         nodePort: {
             set: function (port) {
-                 thisPage.modal.get().element(by.model('node.port')).sendKeys(port)
+                thisPage.modal.get().element(by.model('node.port')).sendKeys(port)
+            }
+        },
+        addButton: {
+            click: function () {
+                thisPage.modal.get().element(by.partialButtonText('Add')).click()
             }
         }
+    };
 
+    this.toasts = {
+        divs: {
+            get: function () {
+                return element(by.id('toast-container')).all(by.tagName('div'))
+            },
+            count: function () {
+                return thisPage.toasts.divs.get().count()
+            }
+        },
+        top: {
+            title: {
+                get: function () {
+                    return thisPage.toasts.divs.get().get(1).getText()
+                }
+            },
+            content: {
+                get: function () {
+                    return thisPage.toasts.divs.get().get(2).getText()
+                }
+            }
+        },
+        any: function () {
+            return thisPage.toasts.divs.count().then(function (count) {
+                return count > 0;
+            });
+        },
+        empty: function () {
+            return thisPage.toasts.divs.count().then(function (count) {
+                return count === 0;
+            });
+        }
+    };
+
+    this.pageHeader = {
+        get: function () {
+            return element(by.css('[class="page-header"]')).getText()
+        }
     }
-
 };
 
 
--- test_e2e/test.spec.js
+++ test_e2e/test.spec.js
@@ -1,48 +1,50 @@
 describe('HPCPM', function () {
+    browser.ignoreSynchronization = true;
+
+    var node = {
+        name: 'integration-test-node',
+        address: 'vps.lel.lu',
+        port: '40001'
+    };
+    var pageTitle = 'HPC Power Management';
 
     var HPCPMPage = require('./pages/hpcpm.page.js');
     page = new HPCPMPage();
     page.get();
-    var nodeName = 'integration-test-node';
-    var pageTitle = 'HPC Power Management';
-    var toasts = element(by.id('toast-container')).all(by.tagName('div'));
 
-    browser.ignoreSynchronization = true;
+    describe('Adding node', function () {
+        it('should show main page', function () {
+            expect(page.title.get()).toBe(pageTitle);
+        });
 
-    it('should show main page', function () {
-        expect(page.title.get()).toBe(pageTitle);
+        it('should show add node modal', function () {
+            page.addNodeButton.click();
+            expect(page.modal.title.get()).toBe('Add new computation node');
+        });
+
+        it('should add new node', function () {
+            page.modal.nodeName.set(node.name);
+            page.modal.nodeAddress.set(node.address);
+            page.modal.nodePort.set(node.port);
+            page.modal.addButton.click();
+
+            browser.wait(page.toasts.any, 5000);
+            expect(page.toasts.top.title.get()).toBe('Success');
+            expect(page.toasts.top.content.get()).toBe('Computation node ' + node.name + ' added successfully');
+
+            browser.driver.sleep(2000);
+            var row = page.nodesTable.nodeRow(node.name).get();
+            expect(row).toBeDefined();
+        });
+
+        it('should make node added toast disappear', function () {
+            browser.wait(page.toasts.empty, 6000);
+        })
     });
-
-    it('should show add node modal', function () {
-        page.addNodeButton.click();
-        expect(page.modal.title.get()).toBe('Add new computation node');
-    });
-
-    it('should add new node', function () {
-        page.modal.nodeName.set(nodeName);
-        page.modal.nodeAddress.set('vps.lel.lu');
-        page.modal.nodePort.set('40001');
-        element(by.css('[class="modal-dialog"]')).element(by.partialButtonText('Add')).click();
-        browser.wait(function () {
-            return toasts.count().then(function (count) {
-                return count > 0;
-            });
-        }, 5000);
-        expect(toasts.get(1).getText()).toBe('Success');
-        expect(toasts.get(2).getText()).toBe('Computation node ' + nodeName + ' added successfully');
-        browser.driver.sleep(2000);
-        var row = page.nodesTable.getNodeRow(nodeName);
-        expect(row).toBeDefined();
-
-    });
-
-
-    it('should make node added toast disappear', function () {
-        browser.wait(function () {
-            return toasts.count().then(function (count) {
-                return count === 0;
-            });
-        }, 5000);
-
+    describe('Node details', function() {
+        it ('should show node details page', function () {
+            page.nodesTable.nodeRow(node.name).nameField().link.click()
+            expect(page.pageHeader.get()).toBe('Node: ' + node.name)
+        })
     })
 });
